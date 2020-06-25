d3a09400fad28b8b7ff6ca8a608b504e2b782a38
hwangbo <taekwonod714@naver.com>
2020-06-25 16:17:11 +0900

    배열에 항목 수정하기

--- src/component/UserList.js
+++ src/component/UserList.js
@@ -1,19 +1,34 @@
 import React from 'react';
 
-function User({ user, onRemove }) {
+function User({user, onRemove, onToggle}) {
     return (
         <div>
-            <b>{user.username}</b> <span>({user.email})</span>
+            <b
+                style={{
+                    cursor: 'pointer',
+                    color: user.active ? 'green' : 'black'
+                }}
+                onClick={() => onToggle(user.id)}
+            >
+                {user.username}
+            </b>
+
+            <span>({user.email})</span>
             <button onClick={() => onRemove(user.id)}>삭제</button>
         </div>
     );
 }
 
-function UserList({ users, onRemove }) {
+function UserList({ users, onRemove, onToggle }) {
     return (
         <div>
             {users.map(user => (
-                <User user={user} key={user.id} onRemove={onRemove}/>
+                <User
+                    user={user}
+                    key={user.id}
+                    onRemove={onRemove}
+                    onToggle={onToggle}
+                />
                 // 각 고유 원소에 key가 있어야만 배열이 업데이트 될 때 효율적으로 렌더링 된다.
             ))}
         </div>
