3575e4da3e18f35c22ee6fe2cacb64b0c9dbcbf3
hwangbo <taekwonod714@naver.com>
2020-06-28 15:44:26 +0900

    feat: useCallback을 사용하여 함수 재사용하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState, useMemo} from 'react';
+import React, {useRef, useState, useMemo, useCallback} from 'react';
 import UserList from "./component/UserList";
 import CreateUser from "./component/CreateUser";
 
@@ -13,13 +13,16 @@ function App() {
         email: ''
     });
     const {username, email} = inputs;
-    const onChange = e => {
-        const {name, value} = e.target;
-        setInputs({
-            ...inputs,
-            [name]: value
-        });
-    };
+    const onChange = useCallback(
+        e=> {
+            const { name, value } = e.target;
+            setInputs({
+                ...inputs,
+                [name]: value
+            });
+        },
+        [inputs]
+    );
     const [users, setUsers] = useState([
         {
             id: 1,
@@ -42,35 +45,38 @@ function App() {
     ]);
 
     const nextId = useRef(4);
-    const onCreate = () => {
+    const onCreate = useCallback(() => {
         const user = {
             id: nextId.current,
             username,
             email
         };
-        setUsers([...users, user]);
+        setUsers(users.concat(user));
 
         setInputs({
             username: '',
-            email: ''
+            email:''
         });
-
         nextId.current += 1;
-    };
+    }, [users, username, email]);
 
-    const onRemove = id => {
-        // user.id가 파라미터로 일치하지 않는 원소만 추출해 새로운 배열을 만듦.
-        // user.id가 id 인 것을 제거한다는 의미.
-        setUsers(users.filter(user => user.id !== id));
-    };
+    const onRemove = useCallback(
+        id => {
+            setUsers(users.filter(user => user.id !== id));
+        },
+        [users]
+    );
 
-    const onToggle = id => {
-        setUsers(
-            users.map(user =>
-                user.id === id ? {...user, active: !user.active} : user
-            )
-        );
-    };
+    const onToggle = useCallback(
+        id => {
+            setUsers(
+                users.map(user =>
+                    user.id === id ? { ...user, active: !user.active } : user
+                )
+            );
+        },
+        [users]
+    );
     const count = useMemo(() => countActiveUsers(users), [users]);
 
     return (
